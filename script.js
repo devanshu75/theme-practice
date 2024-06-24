2269be808e836b2af8f7097bd09193ac31fd32cd
devanshu75 <devanshurana75@gmail.com>
2024-06-24 22:17:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeColor() {
+  document.getElementById("v").style.color = "#ff0000";
+  document.getElementById("i").style.color = "#ff7f00";
+  document.getElementById("b").style.color = "#ffff00";
+  document.getElementById("g").style.color = "#00ff00";
+  document.getElementById("y").style.color = "#0000ff";
+  document.getElementById("o").style.color = "#4b0082";
+  document.getElementById("r").style.color = "#9400d3";
+}
