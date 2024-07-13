197e9717ecb93f7b8c45b54b8bf2d53bb27805a2
devanshu75 <devanshurana75@gmail.com>
2024-07-13 19:47:48 +0530

    Code fixed

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
-const v = document.getElementById("v");
-const i = document.getElementById("i");
-const b = document.getElementById("b");
-const g = document.getElementById("g");
-const y = document.getElementById("y");
-const o = document.getElementById("o");
-const r = document.getElementById("r");
+let h1Element = document.querySelector("h1");
+let h2Element = document.querySelector("h2");
+let h3Element = document.querySelector("h3");
+let h4Element = document.querySelector("h4");
+let h5Element = document.querySelector("h5");
+let h6Element = document.querySelector("h6");
 
-(() => {
-  v.style.color = "rgb(148, 0, 211)";
-  i.style.color = "rgb(75, 0, 130)";
-  b.style.color = "rgb(0, 0, 255)";
-  g.style.color = "rgb(0, 255, 0)";
-  y.style.color = "rgb(255, 255, 0)";
-  o.style.color = "rgb(255, 127, 0)";
-  r.style.color = "rgb(255, 0, 0)";
-})();
+let h1_styles = getComputedStyle(h1Element);
+let h2_styles = getComputedStyle(h2Element);
+let h3_styles = getComputedStyle(h3Element);
+let h4_styles = getComputedStyle(h4Element);
+let h5_styles = getComputedStyle(h5Element);
+let h6_styles = getComputedStyle(h6Element);
 
-function changeColor() {
-  if (v.style.color == "rgb(148, 0, 211)") {
-    v.style.color = "rgb(255, 0, 0)";
+let violet = h1_styles.getPropertyValue("--v");
+let indigo = h2_styles.getPropertyValue("--i");
+let blue = h3_styles.getPropertyValue("--b");
+let green = h4_styles.getPropertyValue("--g");
+let yellow = h4_styles.getPropertyValue("--y");
+let orange = h4_styles.getPropertyValue("--o");
+let red = h4_styles.getPropertyValue("--r");
+
+let theme = 0;
+
+function ThemeSwitch() {
+  if (theme === 0) {
+    darkTheme();
   } else {
-    v.style.color = "rgb(148, 0, 211)";
+    lightTheme();
   }
+}
 
-  if (i.style.color == "rgb(75, 0, 130)") {
-    i.style.color = "rgb(255, 127, 0)";
-  } else {
-    i.style.color = "rgb(75, 0, 130)";
-  }
+function darkTheme() {
+  console.log("Dark Theme", theme);
 
-  if (b.style.color == "rgb(0, 0, 255)") {
-    b.style.color = "rgb(255, 255, 0)";
-  } else {
-    b.style.color = "rgb(0, 0, 255)";
-  }
+  h1Element.style.setProperty("--v", "#ff0000");
+  h2Element.style.setProperty("--i", "#ff7f00");
+  h3Element.style.setProperty("--b", "#ffff00");
+  h4Element.style.setProperty("--g", "#00ff00");
+  h5Element.style.setProperty("--y", "#0000ff");
+  h6Element.style.setProperty("--o", "#4b0082");
+  h6Element.style.setProperty("--r", "#9400d3");
 
-  if (g.style.color == "rgb(0, 255, 0)") {
-    g.style.color = "rgb(0, 0, 255)";
-  } else {
-    g.style.color = "rgb(0, 255, 0)";
-  }
+  theme = 1;
+}
 
-  if (y.style.color == "rgb(255, 255, 0)") {
-    y.style.color = "rgb(0, 0, 255)";
-  } else {
-    y.style.color = "rgb(255, 255, 0)";
-  }
+function lightTheme() {
+  console.log("Light THeme", theme);
 
-  if (o.style.color == "rgb(255, 127, 0)") {
-    o.style.color = "rgb(75, 0, 130)";
-  } else {
-    o.style.color = "rgb(255, 127, 0)";
-  }
+  h1Element.style.setProperty("--v", "#9400d3");
+  h2Element.style.setProperty("--i", "#4b0082");
+  h3Element.style.setProperty("--b", "#0000ff");
+  h4Element.style.setProperty("--g", "#00ff00");
+  h5Element.style.setProperty("--y", "#ffff00");
+  h6Element.style.setProperty("--o", "#ff7f00");
+  h6Element.style.setProperty("--r", "#ff0000");
 
-  if (r.style.color == "rgb(255, 0, 0)") {
-    r.style.color = "rgb(148, 0, 211)";
-  } else {
-    r.style.color = "rgb(255, 0, 0)";
-  }
+  theme = 0;
 }
